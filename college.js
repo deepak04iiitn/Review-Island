649cadcb6a758b6758aea6230d73f15708d17313
deepak04iiitn <148753574+deepak04iiitn@users.noreply.github.com>
2024-01-26 15:10:37 +0530

    Add files via upload

---
+++ college.js
@@ -0,0 +1,31 @@
+function clickCollege() {
+    window.location.href = '/college-form';
+}
+
+function clickHome() {
+    window.location.href = '/index';
+}
+
+function clickHotal() {
+    window.location.href = '/hotal-form';
+}
+
+function clickSchools() {
+    window.location.href = '/schools-form';
+}
+
+function clickRestaurants() {
+    window.location.href = '/restaurants-form';
+}
+
+function clickHospitals() {
+    window.location.href = '/hospitals-form';
+}
+
+function clickTourist() {
+    window.location.href = '/tourist-form';
+}
+
+function clickCoaching() {
+    window.location.href = '/coaching-form';
+}
